53cbac090ec2ca6cbe004fc40e3403ad800986f8
Rodrigo-Elizondo <074907@delvalle-isd.org>
2025-01-13 09:08:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+print('Hi welcome you out chat room this is our chatbot ')
+
+from datetime import datetime
+
+birthday_YMD = input("Please enter your birthday (YYYY-MM-DD): ")
+
+birthday = datetime.strptime(birthday_YMD, "%Y-%m-%d")
+
+today = datetime.today()
+
+age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
+
+print(f"You are {age} years old.")
